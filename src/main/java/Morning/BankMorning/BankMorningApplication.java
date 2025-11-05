0bb3afbd345027c6673323445fda58a2f4efd552
rhodrigo081 <rhodrigo.leandro@gmail.com>
2025-11-05 08:58:49 -0300

    feat: spring feature added

---
+++ src/main/java/Morning/BankMorning/BankMorningApplication.java
@@ -0,0 +1,13 @@
+package Morning.BankMorning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankMorningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankMorningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Morning/BankMorning/BankMorningApplicationTests.java
@@ -0,0 +1,13 @@
+package Morning.BankMorning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankMorningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
